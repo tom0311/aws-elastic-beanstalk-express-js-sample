393ff483dffcd8f88be3e7ff673f593bbf55b274
tom0311 <tom@altade.cn>
2022-09-07 14:50:18 +0000

    2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Night'));
+app.get('/', (req, res) => res.send('Hello Evening!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
